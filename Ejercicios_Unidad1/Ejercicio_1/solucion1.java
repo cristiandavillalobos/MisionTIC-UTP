2a3d8334c68975992a8cccb796db1ba518a8dc84
cristiandavillalobos <cristiandavillalobos@gmail.com>
2021-06-29 20:14:31 -0500

    Actualizacion

---
+++ Ejercicios_Unidad1/Ejercicio_1/solucion1.java
@@ -0,0 +1,10 @@
+/*Realizar la suma, la resta, la división y la multiplicación de dos números
+leídos por teclado y mostrar en pantalla “La <operación> de <número 1>
+y <número 2> es igual a <resultado> */
+public class solucion1 {
+    public static void main(String[] args) {
+        int numero1 = 15;
+        int numero2 = 9;
+        System.out.println("La suma de " + numero1 + " y " + numero2 +" es igual a "+(numero1 + numero2));
+      }
+}
--- Prueba/Prueba.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Prueba {
-    public static void main(String[] args) {
-      int Prueba = (25);
-      String Texto = "Prueba de suma de caracteres";
-      System.out.println(Texto +" "+ Prueba);
-    }
-  }
